0a39d0e3c0d67e8a8ea12a6a96e05035d74b8916
Nicolas Salas V <nikosalas@gmail.com>
2017-05-12 22:46:05 -0300

    Use comint-mode instead of compilation for the server

--- projectile-django.el
+++ projectile-django.el
@@ -5,15 +5,26 @@
 (defvar projectile-django-server-buffer-name "*projectile-django-server*"
   "Name of the server buffer for projectile-django.")
 
-(defvar projectile-django-server-mode-ansi-colors t
-  "Set to t if you want to apply filter color on projectile django server mode.")
+(defvar projectile-django-serve-for-everyone nil
+  "Set to t if you want to bind the server to 0.0.0.0.")
 
-(define-derived-mode projectile-django-server-mode compilation-mode "django-server"
+(defvar projectile-django-default-port 8000
+  "Default port to bind the django server.")
+
+(defvar projectile-django-server-command "runserver"
+  "Command to run the django server.")
+
+(defvar projectile-django-python-interpreter "python"
+  "Path variable to the python interpreter.
+
+Switch to a full route if you want to get it out of an environment.")
+
+(define-derived-mode projectile-django-server-mode comint-mode "django-server"
   "Compilation mode for running django server used by `projectile-django'.
 
 Killing the buffer will terminate its server process."
   ;; (when projectile-django-server-mode-ansi-colors
-    ;; (add-hook 'compilation-filter-hook 'projectile-django-apply-ansi-color nil t))
+  ;; (add-hook 'compilation-filter-hook 'projectile-django-apply-ansi-color nil t))
   (setq-local compilation-scroll-output t)
   (add-hook 'kill-buffer-hook 'projectile-django--kill-server t t)
   (add-hook 'kill-emacs-hook 'projectile-django--kill-server t t))
@@ -23,15 +34,37 @@ Killing the buffer will terminate its server process."
   (expand-file-name (concat (projectile-project-root)
                             "manage.py")))
 
+(defun projectile-django--get-bind-string ()
+  "Get bind string for the server."
+  (if projectile-django-serve-for-everyone
+      (format "0.0.0.0:%d" projectile-django-default-port)
+    (format "%d" projectile-django-default-port)))
+
+(defun projectile-django--assemble-server-command ()
+  "Return the command string after manage.py to run the server."
+  (let ((bind-string (projectile-django--get-bind-string)))
+    (format "%s %s %s"
+            (projectile-django--locate-manage)
+            projectile-django-server-command
+            bind-string)))
+
+(defun projectile-django--get-server-buffer-name ()
+  "Return a suitable buffer name for the django server."
+  (concat "*" (projectile-project-name) "-django-server*"))
+
 (defun projectile-django-server ()
   "Run the django server if it's not running, otherwise switch to its buffer."
   (interactive)
-  (if (member projectile-django-server-buffer-name (mapcar 'buffer-name (buffer-list)))
-      (switch-to-buffer projectile-django-server-buffer-name)
-    (compile (concat "python "
-                     (projectile-django--locate-manage)
-                     " runserver")
-             'projectile-django-server-mode)))
+  (let ((server-buffer-name (projectile-django--get-server-buffer-name)))
+    (if (member server-buffer-name (mapcar 'buffer-name (buffer-list)))
+        (switch-to-buffer server-buffer-name)
+      (set-buffer (apply 'make-comint-in-buffer
+                         (concat (projectile-project-name) "-django-server")
+                         (projectile-django--get-server-buffer-name)
+                         projectile-django-python-interpreter
+                         nil
+                         (split-string-and-unquote (projectile-django--assemble-server-command))))
+      (switch-to-buffer (current-buffer)))))
 
 (defun projectile-django--kill-server ()
   "Kill the current django server."
